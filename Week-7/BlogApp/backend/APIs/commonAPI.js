ea375018c8c168ed298da38006515eab940302b6
HansikaDhannarapu <hansikadhannarapu@gmail.com>
2026-05-06 10:38:42 +0530

    Update commonAPI.js
    updated file path

--- Week-7/BlogApp/backend/APIs/commonAPI.js
+++ Week-7/BlogApp/backend/APIs/commonAPI.js
@@ -2,7 +2,7 @@ import exp from 'express'
 import { UserModel } from '../models/UserModel.js'
 import { hash, compare } from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import { verifyToken } from '../middlewares/verifyToken.js'
+import { verifyToken } from '../middlewares/VerifyToken.js'
 import { upload } from '../config/multer.js'
 const { sign } = jwt
 export const commonApp = exp.Router()
