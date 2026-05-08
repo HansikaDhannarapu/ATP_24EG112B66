44147409a47811efc2ea6e7c4bcd07b79e045466
HansikaDhannarapu <24eg112b66@anurag.edu.in>
2026-05-08 11:55:50 +0530

    Added .env

--- Week-6/EmpManagemnet/backend/server.js
+++ Week-6/EmpManagemnet/backend/server.js
@@ -2,25 +2,37 @@ import exp from "express";
 import { connect } from "mongoose";
 import { empRoute } from "./API/empApp.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = exp();
+
 //add cors middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: "*",
   }),
 );
+
 //body parser middleware
 app.use(exp.json());
+
 //emp api middleware
 app.use("/emp-api", empRoute);
 
 //DB connection
 const connectDB = async () => {
   try {
-    await connect("mongodb://localhost:27017/empdb");
+    await connect(process.env.DB_URL);
+
     console.log("DB connected");
-    app.listen(4000, () => console.log("server listening on port 4000.."));
+
+    const PORT = process.env.PORT || 4000;
+
+    app.listen(PORT, () =>
+      console.log(`server listening on port ${PORT}`)
+    );
   } catch (err) {
     console.log("err in DB connection", err.message);
   }
